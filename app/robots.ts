2a37f64d11dff591343c1dde2c665d47b1d4d549
gunma1004 <hanabank94@naver.com>
2026-08-21 18:22:35 +0900

    도메인 수정

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,11 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  // 🌐 변경할 새 도메인 주소로 수정
-  const baseUrl = 'https://gunmasarang-massage4.vercel.app';
-
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: 'https://gunmasarang.vercel.app/sitemap.xml',
   };
 }
